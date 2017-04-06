9bd79ae1abe47893b643abe68dbf3a673f58554b
Alex Aylesbury <alex@matica.com>
2017-04-06 20:00:04 +0100

    better html

--- matrix.ino
+++ matrix.ino
@@ -80,7 +80,17 @@ void setup() {
   doSpiffs();
   matrix.begin(0x70);  // pass in the address
   setupWiFi();
-  server.on("/", handleRoot);
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([](){
+    if(!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+  /*
+  server.on("/", [](){  
+    handleFileRead("index.html");              
+  });
+  */
   server.on("/scroll",setup_message);
   server.on("/shape",shape_update);
   server.on("/cool", [](){  
@@ -289,3 +299,36 @@ String formatBytes(size_t bytes){
   }
 }
 
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){
+  Serial.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";
+  Serial.println("*handleFileRead: " + path);
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
